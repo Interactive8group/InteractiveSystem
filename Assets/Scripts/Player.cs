be97e2968841b19706d4fdd7e2141741f9d173be
Operanki223 <fgo239@outlook.jp>
2025-11-28 03:45:55 +0900

    被弾したときに吹き出しが出るようにした

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,6 +6,7 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 0.01f;
     [Header("オブジェクトの位置の微調整"), SerializeField] Vector3 pos_config;
     [SerializeField] float moveLimit_up = 0, moveLimit_bottom = 0, moveLimit_left = 0, moveLimit_right = 0;
+    [SerializeField] GameObject fukidasi;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,7 +29,7 @@ public class Player : MonoBehaviour
         {
 
             transform.position = FacePointCollect.instance.GetFaceCenter();
-            Debug.Log("transform.position←" + FacePointCollect.instance.GetFaceCenter());
+            //Debug.Log("transform.position←" + FacePointCollect.instance.GetFaceCenter());
         }
 
         // // 現在の位置を取得
@@ -41,5 +43,26 @@ public class Player : MonoBehaviour
 
         // // 修正した位置を再代入
         // transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            GameManager.instance.TextChange("いた～");
+            ViewFukidashi();
+        }
+    }
+
+    void ViewFukidashi()
+    {
+        fukidasi.SetActive(true);
+        StartCoroutine(HideAfter3Seconds(fukidasi));
+    }
+
+    IEnumerator HideAfter3Seconds(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false); // 非表示にする
     }
 }

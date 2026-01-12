624545819ef139a51485fc2c1eab04eff5f7e1e9
Operanki223 <fgo239kv@outlook.jp>
2026-01-13 03:13:11 +0900

    更新

--- Assets/Scripts/SignalingClient.cs
+++ Assets/Scripts/SignalingClient.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using WebSocketSharp;
-using Unity.WebRTC;
 using System.Collections;
+using Unity.WebRTC;
 
 public class SignalingClient : MonoBehaviour
 {
     public static SignalingClient instance;
+
     private WebSocket ws;
+    private bool isOpen = false;
 
     void Awake()
     {
@@ -15,18 +17,42 @@ public class SignalingClient : MonoBehaviour
 
     IEnumerator Start()
     {
-        ws = new WebSocket("ws://192.168.11.18:3000"); // ← サーバPCのIP
-        ws.OnMessage += (s, e) => StartCoroutine(ProcessMessage(e.Data));
+        ws = new WebSocket("ws://192.168.11.18:3000"); // サーバPCのIP
+
+        ws.OnOpen += (s, e) =>
+        {
+            Debug.Log("WebSocket Open");
+            isOpen = true;
+        };
+
+        ws.OnMessage += (s, e) =>
+        {
+            Debug.Log("Receive signaling: " + e.Data);
+            StartCoroutine(ProcessMessage(e.Data));
+        };
+
+        ws.OnError += (s, e) =>
+        {
+            Debug.LogError("WebSocket Error: " + e.Message);
+        };
+
+        ws.OnClose += (s, e) =>
+        {
+            Debug.LogWarning("WebSocket Closed");
+            isOpen = false;
+        };
+
         ws.Connect();
 
-        // WebRTC / MediaPipe 初期化待ち
-        yield return new WaitForSeconds(1.0f);
+        // ★ WebSocket が Open になるまで絶対に待つ
+        yield return new WaitUntil(() => isOpen);
 
-        // ★ これが無いと一生映らない
         Debug.Log("Create Offer");
+
         yield return WebRTCManager.instance.CreateOfferCoroutine(offer =>
         {
             Debug.Log("Send Offer");
+
             Send(JsonUtility.ToJson(new SignalingMessage
             {
                 type = "offer",
@@ -37,18 +63,24 @@ public class SignalingClient : MonoBehaviour
 
     public void Send(string message)
     {
+        if (!isOpen)
+        {
+            Debug.LogWarning("WebSocket not open yet. Skip Send.");
+            return;
+        }
+
         Debug.Log("Send signaling: " + message);
         ws.Send(message);
     }
 
     IEnumerator ProcessMessage(string msg)
     {
-        Debug.Log("Receive signaling: " + msg);
         var obj = JsonUtility.FromJson<SignalingMessage>(msg);
 
         if (obj.type == "offer")
         {
             Debug.Log("Receive Offer");
+
             RTCSessionDescription offer = new RTCSessionDescription
             {
                 type = RTCSdpType.Offer,
@@ -56,9 +88,11 @@ public class SignalingClient : MonoBehaviour
             };
 
             yield return WebRTCManager.instance.SetRemoteDescriptionCoroutine(offer);
+
             yield return WebRTCManager.instance.CreateAnswerCoroutine(answer =>
             {
                 Debug.Log("Send Answer");
+
                 Send(JsonUtility.ToJson(new SignalingMessage
                 {
                     type = "answer",
@@ -69,16 +103,19 @@ public class SignalingClient : MonoBehaviour
         else if (obj.type == "answer")
         {
             Debug.Log("Receive Answer");
+
             RTCSessionDescription answer = new RTCSessionDescription
             {
                 type = RTCSdpType.Answer,
                 sdp = obj.sdp
             };
+
             yield return WebRTCManager.instance.SetRemoteDescriptionCoroutine(answer);
         }
         else if (obj.type == "ice")
         {
             Debug.Log("Receive ICE");
+
             RTCIceCandidate candidate = new RTCIceCandidate(
                 new RTCIceCandidateInit
                 {
@@ -87,6 +124,7 @@ public class SignalingClient : MonoBehaviour
                     sdpMLineIndex = obj.sdpMLineIndex
                 }
             );
+
             WebRTCManager.instance.AddIceCandidate(candidate);
         }
     }
--- Assets/Scripts/WebRTCManager.cs
+++ Assets/Scripts/WebRTCManager.cs
@@ -5,30 +5,42 @@ using System.Collections;
 public class WebRTCManager : MonoBehaviour
 {
     public static WebRTCManager instance;
+
     private RTCPeerConnection pc;
     private VideoStreamTrack localTrack;
 
     void Awake()
     {
         instance = this;
-        //WebRTC.Initialize();
+
+        // WebRTC.Initialize(); ← 既に別で呼んでいるなら不要
+
         pc = new RTCPeerConnection();
 
-        pc.OnIceCandidate = c =>
+        pc.OnIceCandidate = candidate =>
         {
-            if (c != null)
-            {
-                SignalingClient.instance.Send(
-                    JsonUtility.ToJson(new SignalingClient.SignalingMessage
-                    { type = "ice", candidate = c.Candidate, sdpMid = c.SdpMid, sdpMLineIndex = c.SdpMLineIndex.Value })
-                );
-            }
+            if (candidate == null) return;
+            if (SignalingClient.instance == null) return;
+
+            SignalingClient.instance.Send(
+                JsonUtility.ToJson(new SignalingClient.SignalingMessage
+                {
+                    type = "ice",
+                    candidate = candidate.Candidate,
+                    sdpMid = candidate.SdpMid,
+                    sdpMLineIndex = candidate.SdpMLineIndex.Value
+                })
+            );
         };
 
         pc.OnTrack = e =>
         {
+            Debug.Log("OnTrack called");
+
             if (e.Track is VideoStreamTrack videoTrack)
+            {
                 EnemyFaceReceiver.instance.SetRemoteVideo(videoTrack);
+            }
         };
     }
 
@@ -43,12 +55,16 @@ public class WebRTCManager : MonoBehaviour
         var op = pc.CreateOffer();
         yield return op;
 
-        if (!op.IsError)
+        if (op.IsError)
         {
-            var desc = op.Desc;                 // ここを修正
-            yield return pc.SetLocalDescription(ref desc);
-            callback?.Invoke(desc);
+            Debug.LogError("CreateOffer Error");
+            yield break;
         }
+
+        var desc = op.Desc;
+        yield return pc.SetLocalDescription(ref desc);
+
+        callback?.Invoke(desc);
     }
 
     public IEnumerator CreateAnswerCoroutine(System.Action<RTCSessionDescription> callback)
@@ -56,20 +72,35 @@ public class WebRTCManager : MonoBehaviour
         var op = pc.CreateAnswer();
         yield return op;
 
-        if (!op.IsError)
+        if (op.IsError)
         {
-            var desc = op.Desc;                 // ここを修正
-            yield return pc.SetLocalDescription(ref desc);
-            callback?.Invoke(desc);
+            Debug.LogError("CreateAnswer Error");
+            yield break;
         }
-    }
 
+        var desc = op.Desc;
+        yield return pc.SetLocalDescription(ref desc);
+
+        callback?.Invoke(desc);
+    }
 
     public IEnumerator SetRemoteDescriptionCoroutine(RTCSessionDescription desc)
     {
         var op = pc.SetRemoteDescription(ref desc);
         yield return op;
+
+        if (op.IsError)
+        {
+            Debug.LogError("SetRemoteDescription Error");
+        }
+        else
+        {
+            Debug.Log("SetRemoteDescription done");
+        }
     }
 
-    public void AddIceCandidate(RTCIceCandidate candidate) => pc.AddIceCandidate(candidate);
+    public void AddIceCandidate(RTCIceCandidate candidate)
+    {
+        pc.AddIceCandidate(candidate);
+    }
 }

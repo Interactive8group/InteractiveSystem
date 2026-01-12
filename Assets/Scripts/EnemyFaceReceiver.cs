3194d4759252acd6e773faae8d82017cbe5c4ef2
Operanki223 <fgo239kv@outlook.jp>
2026-01-13 03:49:44 +0900

    更新

--- Assets/Scripts/EnemyFaceReceiver.cs
+++ Assets/Scripts/EnemyFaceReceiver.cs
@@ -7,13 +7,38 @@ public class EnemyFaceReceiver : MonoBehaviour
     public static EnemyFaceReceiver instance;
 
     [SerializeField] private RawImage enemyImage;
-    private VideoStreamTrack remoteTrack;
 
-    void Awake() => instance = this;
+    private RenderTexture renderTexture;
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     public void SetRemoteVideo(VideoStreamTrack track)
     {
-        remoteTrack = track;
-        track.OnVideoReceived += tex => enemyImage.texture = tex;
+        Debug.Log("SetRemoteVideo called");
+
+        // ★ RenderTexture を作る（これが超重要）
+        renderTexture = new RenderTexture(1280, 720, 0);
+        renderTexture.Create();
+
+        enemyImage.texture = renderTexture;
+
+        // ★ 受信フレームを必ずコピー
+        track.OnVideoReceived += tex =>
+        {
+            if (renderTexture == null) return;
+            Graphics.Blit(tex, renderTexture);
+        };
+    }
+
+    void OnDestroy()
+    {
+        if (renderTexture != null)
+        {
+            renderTexture.Release();
+            renderTexture = null;
+        }
     }
 }
